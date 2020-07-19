2d4e1c93984a1ac77890a94236f96c2876d7d45e
Marat Krahin <paparasum@gmail.com>
2020-07-19 17:43:33 +0300

    HW Task1

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+package Calculator;
+
+import java.util.function.*;
+
+public class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+
+    BinaryOperator<Integer> plus = Integer::sum;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? x :  x / y; // делить на 0 нельзя, поэтому
+    // возвращаем х если y == 0
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+
+
+}
